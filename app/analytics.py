0a2434938912e968f487a24a96bf9bc9dd0a43f7
Matthew deHaas <mattdehaas28@gmail.com>
2025-06-26 16:59:43 -0400

    Download all schools' data to a json file.

--- app/analytics.py
+++ app/analytics.py
@@ -3,6 +3,45 @@ from bs4 import BeautifulSoup
 import json
 
 
+# A dictionary of all the schools that use Sidearm Sports to write their website
+schools = {
+
+    "University of Alberta Golden Bears & Pandas": "bearsandpandas.ca",
+    "Brandon UniversityBobcats": "gobobcats.ca",
+    "University of British Columbia Thunderbirds": "gothunderbirds.ca",
+    "University of British Columbia – Okanagan Heat": "goheat.ca",
+    "University of Calgary Dinos": "godinos.com",
+    "University of Lethbridge Pronghorns": "gohorns.ca",
+    "University of Manitoba Bisons": "gobisons.ca",
+    "Mount Royal University Cougars": "mrucougars.com",
+    "University of Northern British Columbia Timberwolves": "unbctimberwolves.com",
+    "University of Regina Cougars & Rams": "reginacougars.com",
+    "University of Saskatchewan Huskies": "huskies.usask.ca",
+    "Trinity Western University Spartans": "gospartans.ca",
+    "University of Winnipeg Wesmen": "wesmen.ca",
+    "University of Victoria Vikes": "govikesgo.com",
+
+    "Algoma University Thunderbirds": "algomathunderbirds.ca",
+    "Brock University Badgers": "gobadgers.ca",
+    "University of Guelph Gryphons": "gryphons.ca",
+    "McMaster University Marauders": "marauders.ca",
+    "Nipissing University Lakers": "nulakers.ca",
+    "Ontario Tech Ridgebacks": "goridgebacks.com",
+    "Queen’s University Gaels": "gogaelsgo.com",
+    "TMU Bold": "tmubold.ca",
+    "University of Toronto Varsity Blues": "varsityblues.ca",
+    "University of Waterloo Warriors": "athletics.uwaterloo.ca",
+    "Western University Mustangs": "WesternMustangs.ca",
+    "Wilfrid Laurier University Golden Hawks": "laurierathletics.com",
+    "University of Windsor Lancers": "golancers.ca",
+    "York University Lions": "yorkulions.ca",
+
+    "Bishop’s University Gaiters": "gaiters.ca",
+    "McGill University Redmen & Martlets": "mcgillathletics.ca"
+
+}
+
+
 # Returns a dictionary where the keys are the athlete ids and values are their names
 # Helper for the get_athlete_detail()
 def get_sport_data(school_url, sport):
@@ -23,12 +62,12 @@ def get_sport_data(school_url, sport):
         
         # Personal info
         player_field = soup.find("div", {"class": "sidearm-roster-player-fields"})
-        
-        span_eles = [span.get_text() for span in list(player_field.find_all("span"))]
+        if player_field is not None:
+            span_eles = [span.get_text() for span in list(player_field.find_all("span"))]
 
-        detail_dict = dict(zip(span_eles[::2], span_eles[1::2]))
-        for key, val in detail_dict.items():
-            athlete[key] = val
+            detail_dict = dict(zip(span_eles[::2], span_eles[1::2]))
+            for key, val in detail_dict.items():
+                athlete[key] = val
             
         # Image(s) (if any)
         athlete["images"] = []
@@ -44,12 +83,11 @@ def get_sport_data(school_url, sport):
             historical_imgs = soup.find_all("div", {"class": "sidearm-roster-player-image-historical"})    
             for img in historical_imgs:
                 short_image_url = img.attrs["style"]
-                print(short_image_url)
                 if short_image_url.find("?") != -1:
                     short_image_url = short_image_url[short_image_url.index("'") + 1:short_image_url.index("?")]
                     athlete["images"].append(f"https://{school_url}{short_image_url}")
 
-        print(athlete)
+        print(f"name: {athlete["name"]}, sport: {athlete["sport"]}, school: {athlete["school_url"]}")
 
     return athletes
 
@@ -57,7 +95,7 @@ def get_sport_data(school_url, sport):
 # Gets all of the data from all of the sports offered at the given school 
 def get_school_data(school_url):
 
-    soup = BeautifulSoup(requests.get(f"https://{school_url}", 
+    soup = BeautifulSoup(requests.get(f"https://{school_url}/index.aspx", 
                                       headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}).content, "html.parser")  
 
     sports = list()
@@ -67,20 +105,38 @@ def get_school_data(school_url):
         if href and href.find("/sports/") != -1 and href.count("/") == 2:
             sports.append(a["href"][a["href"].rfind("/") + 1:])
 
+    sports = list(set(sports))
+
+    print(sports)
+
     school_data = dict()
     for sport in sports:
         school_data[sport] = get_sport_data(school_url, sport)
-        print(school_data[sport])
+        print(f"\n\n\nDONE {sport}\n\n\n")
 
     return school_data
 
 
+def get_schools_data():
+    schools_data = dict()
+    for school, url in schools.items():
+        schools_data[school] = get_school_data(url)
+    return schools_data
 
 
-with open("data/ubc_data.json", "w") as f:
-    json.dump(get_school_data("gothunderbirds.ca"), f, indent=4)
+
+
+# scrapes all of the available data from every school in the 'schools' dictionary
+def scrape_all_schools():
+    with open("data/all_schools_data.json", "w") as f:
+            json.dump(get_schools_data(), f, indent=4)
 
 
 
+# Reads the json file generated by the data scraped from the available schools
+def get_school_json_data():
+    with open("data/all_schools_data.json", "r") as f:
+        return json.load(f)
 
 
+print(get_school_json_data())
--- docs/TODO.md
+++ docs/TODO.md
@@ -1,20 +1,27 @@
 # Roadmap
 
-### 1. Gather athlete images and info
+### Gather athlete images and info
 - [x] Get the list of names from first sport page (find a better way of indexing the html element containing the info than what is currently implemented)
 - [x] Retrieve relevant data from each athlete's page:
   - [x] Historical images
   - [x] Available personal data
-- [ ] Repeat for other schools
-### 2. Setup the Web Server
+- [ ] List all universities that are made by Sidearm Sports and get their links from [here](https://cusn.ca/u-sports-map/)
+- [ ] Add all of scraped data to a json file for fast reading
+- [ ] Download all headshots
+- [ ] Add the local directory location of the images to each athlete's dictionary
+- [ ] Create a model.py file and a sqlalchemy schema
+- [ ] Store in database
+- [ ] Ensure that the code can be run again if needed
+
+### Add CNN
+- [ ] Figure out how to run the CNN code properly
+- [ ] Run the facial attribute CNN on the images
+- [ ] Merge these attributes with the each athlete's data
+- [ ] Ensure that the code can be run again if needed
+
+### Setup the Web Server
 - [x] Write flask boiler plate
   - [x] Front/back end
   - [x] Database
-- [ ] Write a schema
 - [ ] Implement a page that sorts athletes by sport, gender, hometown, etc.
 - [ ] Clicking on an athlete will expand them with a modal ui, showing all their data like the original website
-
-### 3. Add CNN
-- [ ] Run the facial attribute CNN on the images
-- [ ] Merge these attributes with the each athlete's data
-- [ ] Add to the website
