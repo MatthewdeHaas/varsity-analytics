aeb2bfaf9d97ed398f0b6ce6603f7d16876bad2c
Matthew deHaas <mattdehaas28@gmail.com>
2025-06-25 22:50:50 -0400

    Finish scraping implementation for a single university.

--- app/analytics.py
+++ app/analytics.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-
+import json
 
 
 # Returns a dictionary where the keys are the athlete ids and values are their names
@@ -15,14 +15,13 @@ def get_sport_data(school_url, sport):
                profiles]
     ids = [i[i.index("rp_id=") + len("rp_id="):i.index("rp_id=") + len("rp_id=") + 5] for i in profiles]
 
-    athletes = [{"id": ids[i], "name": names[i], "gender": genders[i]} for i in range(len(ids))]
+    athletes = [{"id": ids[i], "name": names[i], "gender": genders[i], "school_url": school_url, "sport": sport} for i in range(len(ids))]
 
 
     for athlete in athletes:
         soup = BeautifulSoup(requests.get(f"https://{school_url}/sports/{sport}/roster/{athlete['name']}/{athlete['id']}").content, "html.parser")
         
         # Personal info
-
         player_field = soup.find("div", {"class": "sidearm-roster-player-fields"})
         
         span_eles = [span.get_text() for span in list(player_field.find_all("span"))]
@@ -50,18 +49,38 @@ def get_sport_data(school_url, sport):
                     short_image_url = short_image_url[short_image_url.index("'") + 1:short_image_url.index("?")]
                     athlete["images"].append(f"https://{school_url}{short_image_url}")
 
+        print(athlete)
 
     return athletes
 
 
-
-get_sport_data("gothunderbirds.ca", "cross-country")
-
-
-
-
-
-
 # Gets all of the data from all of the sports offered at the given school 
-def get_school_data(school):
-    pass
+def get_school_data(school_url):
+
+    soup = BeautifulSoup(requests.get(f"https://{school_url}", 
+                                      headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}).content, "html.parser")  
+
+    sports = list()
+
+    for a in soup.find_all("a"):
+        href = a.get("href")
+        if href and href.find("/sports/") != -1 and href.count("/") == 2:
+            sports.append(a["href"][a["href"].rfind("/") + 1:])
+
+    school_data = dict()
+    for sport in sports:
+        school_data[sport] = get_sport_data(school_url, sport)
+        print(school_data[sport])
+
+    return school_data
+
+
+
+
+with open("data/ubc_data.json", "w") as f:
+    json.dump(get_school_data("gothunderbirds.ca"), f, indent=4)
+
+
+
+
+
